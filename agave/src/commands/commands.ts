92e97e4a78e49e2531aadb8f4355cd3d37fdf83f
Rob Little <rlittletht@users.noreply.github.com>
2022-02-21 14:58:04 -0800

    Reformat code

--- agave/src/commands/commands.ts
+++ agave/src/commands/commands.ts
@@ -5,37 +5,40 @@
 
 /* global global, Office, self, window */
 
-Office.onReady(() => {
-  // If needed, Office.js is ready to be called
+Office.onReady(() =>
+{
+    // If needed, Office.js is ready to be called
 });
 
 /**
  * Shows a notification when the add-in command is executed.
  * @param event
  */
-function action(event: Office.AddinCommands.Event) {
-  const message: Office.NotificationMessageDetails = {
-    type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-    message: "Performed action.",
-    icon: "Icon.80x80",
-    persistent: true,
-  };
+function action(event: Office.AddinCommands.Event)
+{
+    const message: Office.NotificationMessageDetails = {
+        type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+        message: "Performed action.",
+        icon: "Icon.80x80",
+        persistent: true,
+    };
 
-  // Show a notification message
-  Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
+    // Show a notification message
+    Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
 
-  // Be sure to indicate when the add-in command function is complete
-  event.completed();
+    // Be sure to indicate when the add-in command function is complete
+    event.completed();
 }
 
-function getGlobal() {
-  return typeof self !== "undefined"
-    ? self
-    : typeof window !== "undefined"
-    ? window
-    : typeof global !== "undefined"
-    ? global
-    : undefined;
+function getGlobal()
+{
+    return typeof self !== "undefined"
+               ? self
+               : typeof window !== "undefined"
+                 ? window
+                 : typeof global !== "undefined"
+                   ? global
+                   : undefined;
 }
 
 const g = getGlobal() as any;
