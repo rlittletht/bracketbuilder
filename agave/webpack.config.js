948c6b71e7205161bd48beb754fefe889b3c356c
Rob Little <rlittletht@users.noreply.github.com>
2022-05-11 09:35:48 -0700

    update for deployment to azure and office 365

--- agave/webpack.config.js
+++ agave/webpack.config.js
@@ -6,7 +6,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://twbbldcdnendpoint.azureedge.net/";
 
 async function getHttpsOptions()
 {
