9f2dacc156b05de951b00a4fd3ca1a1a6ab5f17f
Rob Little <rlittletht@users.noreply.github.com>
2023-05-27 12:31:10 -0700

    update version

--- agave/src/taskpane/components/App.tsx
+++ agave/src/taskpane/components/App.tsx
@@ -74,7 +74,7 @@ export interface AppState
 
 export default class App extends React.Component<AppProps, AppState>
 {
-    static version: string = "1.0.5.1";
+    static version: string = "1.0.10.0";
 
     m_appContext: AppContext;
 
